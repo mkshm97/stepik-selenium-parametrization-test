da17343e1a3a2050182b931b08901562f989d271
mkshm97 <max.korchagin97@gmail.com>
2020-08-04 15:24:03 +0300

    time.sleep changed

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_items(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(30)
     btn = browser.find_element_by_class_name('btn-add-to-basket')
     print("\nТекст кнопки: " + btn.text)
     assert len(btn.text) > 0, "Кнопка не найдена"
